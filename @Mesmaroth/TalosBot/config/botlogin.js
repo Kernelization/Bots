6c6c953859be559dfeb51a897909494908122c79
Robert <Mesmaroth666@live.com>
2017-05-12 01:28:28 -0700

    Added config file

---
+++ @Mesmaroth/TalosBot/config/botlogin.js
@@ -0,0 +1,6 @@
+// For token: https://discordapp.com/developers/applications/me
+// For Twitch: https://www.twitch.tv/settings/connections
+module.exports = {
+	token: "TOKEN_KEY",
+	twitchClientID: "TWITCH_CLIENT_ID"
+}
