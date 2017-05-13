7224f13fb4593aac3ff8c547d5641c99212419d3
Unknown <dansps4@outlook.com>
2017-05-13 01:31:36 +0100

    updated README and made it so user doesn't change info in config.json

--- @diredan/README.md
+++ @diredan/README.md
@@ -3,11 +3,9 @@
 1) `git clone https://github.com/DireDan/DiscordJS_Bot.git`
 2) `cd DiscordJS_Bot`
 3) `npm install`
-4) open up config.json
-5) replace INSERT_TOKEN with your bots token
-6) replace INSERT_YOUR_USER_ID with your user id
-7) `node index.js`
-8) success!
+4) `node index.js`
+5) follow instructions in terminal
+6) success!
 
 
 ## Credits
--- @diredan/index.js
+++ @diredan/index.js
@@ -1,13 +1,34 @@
+
 /*
  * importing discord.js module, creating client object,
  * and getting config file :D
  */
 const Discord = require("discord.js");
 const Client = new Discord.Client();
-const Config = require("./config.json");
 
 // run our event loader :)
 require("./eventloader/loader.js").run(Client);
 
-// log into the bot
-Client.login(Config.token);
+const fs = require("fs");
+var Config = require("./config.json");
+if (Config.token === "INSERT_TOKEN" || Config.prefix === "INSERT_PREFIX" ||
+    Config.owner === "INSERT_YOUR_USER_ID") {
+  let readline = require("readline");
+  let rl = readline.createInterface({input: process.stdin, output: process.stdout });
+  rl.question("Enter bot token: ", token => {
+    rl.question("Enter bot prefix: ", prefix => {
+      rl.question("Enter your user id: ", userid => {
+        Config.token = token;
+        Config.prefix = prefix;
+        Config.owner = userid;
+        fs.writeFile("./config.json", JSON.stringify(Config, null, 2), () => {
+          // log into the bot
+          Client.login(Config.token);
+        });
+      });
+    });
+  })
+} else {
+  // log into the bot
+  Client.login(Config.token);
+}
