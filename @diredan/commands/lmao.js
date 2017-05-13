d90dfb400c42b8faae213e5c2bb5db8af5ed5835
Unknown <dansps4@outlook.com>
2017-05-13 02:12:27 +0100

    updated  lmao to have 2 pictures

--- @diredan/commands/lmao.js
+++ @diredan/commands/lmao.js
@@ -5,6 +5,7 @@ module.exports = {
   run : (args, Client, msg) => {
     msg.delete();
     msg.channel.send(":regional_indicator_l: :regional_indicator_m: :regional_indicator_a: :regional_indicator_o: :joy: :joy:");
+    msg.channel.send("http://i.imgur.com/aXbRgSk.jpg\nhttp://i.imgur.com/7WxC0ZA.png");
   },
   usage : () => {
     return "lmao";
